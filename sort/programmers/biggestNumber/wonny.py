2ca24a4eda975b1c224500c21256ffc3e363baae
git-gardener <59333657+git-gardener@users.noreply.github.com>
2021-07-12 04:58:16 +0900

    Update wonny.py

--- sort/programmers/biggestNumber/wonny.py
+++ sort/programmers/biggestNumber/wonny.py
@@ -1,22 +1,16 @@
 def solution(numbers):
     
     num_str = []
-    num_str2 = {}
+    num_str2 = []
     num_str3 = []
     answer = ''    
     
-    for x in numbers :
-        num_str.append(str(x))
-    num_str.sort(reverse=True)
-
-    for x in num_str:
-        num_str2[int(''.join(x))/(10**(len(x))-1)] = len(x)
+    numbers = list(map(str,numbers))
+    numbers.sort(reverse=True,key = lambda x:int(x)/(10**len(x)-1))
     
-    num_str3 = sorted(num_str2.keys(),reverse=True)
-      
-    for x in num_str3:
-        print(x,(10**num_str2[x]),x*(10**num_str2[x]-1),int(x*(10**num_str2[x])))
-        answer += str(int(x*(10**num_str2[x])))
-                  
-    return answer
+    for x in numbers :
+        answer += x
+    if answer.replace("0","") == "" :
+        answer = "0"
 
+    return answer

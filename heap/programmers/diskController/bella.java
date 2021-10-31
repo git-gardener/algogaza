d1dfe5ce6d1d7721b58439e7ed13e34e33f3f72b
jeongyeonkim <jeongy616@naver.com>
2021-11-01 00:22:58 +0900

    modify heap/programmers/diskController/bella.java

--- heap/programmers/diskController/bella.java
+++ heap/programmers/diskController/bella.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        Arrays.sort(jobs[0]);
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         boolean[] visited = new boolean[jobs.length];
@@ -13,7 +13,16 @@ class Solution {
         int tm = 0;
         int st = jobs[0][0];
 
+        for(int i = 1 ; i < jobs.length; i++){
+            if(visited[i] == false && jobs[i][0] == jobs[0][0]){
+                pq.add(jobs[i][1]);
+                visited[i] = true;
+                st += jobs[i][0];
+            }
+        }
+
         while(!pq.isEmpty()){
+            //System.out.println(pq);
             tm += pq.poll();
             answer += tm;
             for(int i = 1 ; i < jobs.length; i++){
